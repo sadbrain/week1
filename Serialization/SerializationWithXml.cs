20ac7fe9c7bd077ac82d5a5b4a4a2a7b4af5c1ff
sadbrain <102611338+sadbrain@users.noreply.github.com>
2023-05-16 01:38:18 +0700

    Update SerializationWithXml.cs

--- Serialization/SerializationWithXml.cs
+++ Serialization/SerializationWithXml.cs
@@ -1,6 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using System;
 using static System.Console;
+
 using System.IO;
 using System.Xml.Serialization;
 
